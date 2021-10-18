9bc7e4be08996dc1fb8d7a7217802f4cdf58e87b
Yosuke Tanigawa <ytanigaw@stanford.edu>
2021-10-17 21:13:40 -0700

    add reference prep notebooks

---
+++ reference_data_prep/paramters.sh
@@ -0,0 +1,5 @@
+
+data_dir <- '@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@'
+annotation_exome_f <- 'exome/annotation/20201025_exome_oqfe_2020/ukb24983_exomeOQFE.annotation.20210108.compact.tsv.gz'
+annotation_array_f <- 'array-combined/annotation/annotation_20201012/ukb24983_cal_hla_cnv.annot_compact_20201023.tsv.gz'
+annotation_array_exome_f <- 'array-exome-combined/annotation/20210111/ukb24983_cal_hla_cnv_exomeOQFE.annot_20210111.tsv.gz'
